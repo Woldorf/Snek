47a0a3596082d54d4ec0233d04ee91a9d00732cc
Woldorf <humphries.laughter@gmail.com>
2021-08-10 11:27:10 -0600

    Added credits screen and fixed some bugs as well as optimized text display

--- Snek_1.2.0.py
+++ Snek_1.2.0.py
@@ -2,20 +2,24 @@
 Made and produced by Broken Keyboard Studios, all rights reserved.
 Feel free to edit sound volumes located on lines 64 and 65.
 """
+#Working on:
+#Adding more credits
+#
+
 #!/usr/bin/python
 import random,sys,pygame
 from pygame.locals import *
 pygame.init()
 pygame.mixer.init()
-pygame.display.set_caption('Dual Snakes - BrokenKeyboard Studios')
+pygame.display.set_caption("Dual Snakes")
 GameScreen = pygame.display.set_mode((0,0))
 pygame.display.toggle_fullscreen()
 
 GAMEWIDTH=pygame.display.Info().current_w
 GAMEHEIGHT=pygame.display.Info().current_h
 
-FONT = pygame.font.Font('freesansbold.ttf', 18)
-BIGFONT = pygame.font.Font("freesansbold.ttf",26)
+FONT = 18
+BIGFONT = 26
 
 #Colors:
 #             R    G    B
@@ -61,7 +65,7 @@ GameMusicChannel = pygame.mixer.Channel(2)
 WinnerAppleSound = pygame.mixer.Sound("SnekFiles/WinningApple.wav")
 #Set game music volume lower:
 GameMusicChannel.set_volume(0.2)
-#EatingAppleChannel.set_volume(0.0)
+EatingAppleChannel.set_volume(0.2)
 
 class Text:
     def __init__(self,Text,Color,Size, Y = None, X = None,):
@@ -70,15 +74,16 @@ class Text:
 
         if X == None:
             self.X = int(GAMEWIDTH/2)
-        if Y != None:
+        else:
+            self.X = X
+
+        if Y == None:
+            self.Y = int(GAMEHEIGHT/2)
+        else:
             self.Y = Y
             self.Obj.center = (self.X,self.Y)
 
-    def UpdateScreen(self,ScrollSpeed = None):
-        if ScrollSpeed != None:
-            self.Y -= ScrollSpeed
-            self.Obj.center = (self.X,self.Y)
-
+    def UpdateScreen(self):
         GameScreen.blit(self.Text,self.Obj)
 
 #Snek Class:
@@ -107,13 +112,14 @@ class Snek:
                 pygame.draw.rect(GameScreen, self.InnerColor, SnekInnerSegmentRect)
 
     def DrawLable(self):
-        scoreSurf = FONT.render("Snek"+str(self.Player)+" Length:"+str(len(self.CordsList)),True, self.LableColor)
-        scoreRect = scoreSurf.get_rect()
         if self.Player == 1:
-            scoreRect.topleft = (10,10)
+            Score = Text("Snek "+str(self.Player)+" Length: "+str(len(self.CordsList)),self.LableColor,FONT)
+            Score.Obj.topleft = (20 ,20)
         else:
-            scoreRect.topright = ((GAMEWIDTH - 10),10)
-        GameScreen.blit(scoreSurf,scoreRect)
+            Score = Text("Snek "+str(self.Player)+" Length: "+str(len(self.CordsList)),self.LableColor,FONT)
+            Score.Obj.topright = (GAMEWIDTH-20 ,20)
+
+        Score.UpdateScreen()
 
     def ChangeDirection(self,Key):
         if self.Player == 1:
@@ -217,10 +223,8 @@ def drawGrid():
         pygame.draw.line(GameScreen, DARKGRAY, (0, y), (GAMEWIDTH, y))
 
 def drawPaused():
-    Words = BIGFONT.render("GAME IS PAUSED",True,RED)
-    WordsRect = Words.get_rect()
-    WordsRect.center = (int(GAMEWIDTH/2),int(GAMEHEIGHT/2))
-    GameScreen.blit(Words,WordsRect)
+    Words = Text("GAME IS PAUSED",RED,BIGFONT,int(GAMEHEIGHT/2),int(GAMEWIDTH/2))
+    Words.UpdateScreen()
 
 def drawFruits(AppleCords):
     x=AppleCords['x'] * CELLSIZE
@@ -238,24 +242,17 @@ def drawFruits(AppleCords):
         pygame.draw.rect(GameScreen,RAINBOW,FruitRect)
 
 def drawStartScreen(Snek1,Snek2):
-    titleWords = BIGFONT.render('DUAL SNEK', True, WHITE, None)
-    EnterMessage = BIGFONT.render('PRESS ENTER OR SPACE TO PLAY',True,RED,None)
-    QuitMessage = BIGFONT.render("ESC TO QUIT",True,RED,None) 
+    SettingsGap = 20
+    titleWords = Text("DUAL SNEK",ORANGE,BIGFONT,GAMEHEIGHT/2)
+    EnterMessage = Text("ENTER OR SPACE TO BEGIN",RED,BIGFONT,GAMEHEIGHT/3)
+    QuitMessage = Text("ESCAPE TO QUIT",RED,BIGFONT,GAMEHEIGHT/5)
 
-    StatsWords = FONT.render(" STATS ",True,WHITE)
-    StatsRect = pygame.draw.rect(GameScreen,RED,(10,(GAMEHEIGHT-(GAMEHEIGHT/8)),90,100))
-    ControlsWords = FONT.render(" CONTROLS ",True,WHITE)
-    ControlsRect = pygame.draw.rect(GameScreen,RED,(10,(GAMEHEIGHT-(GAMEHEIGHT/20)),90,100))
-
-    #Display first message
-    TitleRect = titleWords.get_rect()
-    TitleRect.center = (int(GAMEWIDTH/2), int(GAMEHEIGHT/2))
-    #Display "Enter" message
-    EnterRect=EnterMessage.get_rect()
-    EnterRect.center = (int(GAMEWIDTH/2),int((GAMEHEIGHT - (GAMEHEIGHT/3))))
-    #Display the Quit message
-    QuitRect = QuitMessage.get_rect()
-    QuitRect.center = (int(GAMEWIDTH/2),int((GAMEHEIGHT - (GAMEHEIGHT/4))))
+    StatsWords = Text("STATS",WHITE,FONT,GAMEHEIGHT - 50)
+    StatsWords.X = SettingsGap + StatsWords.Obj.width/2
+    ControlsWords = Text("CONTROLS",WHITE,FONT,StatsWords.Obj.bottom + StatsWords.Obj.height)
+    ControlsWords.X = SettingsGap + ControlsWords.Obj.width/2
+    #Credits = Text("CREDITS",WHITE,FONT,ControlsWords.Obj.bottom+ControlsWords.Obj.height)
+    #Credits.X = SettingsGap + Credits.Obj.width/2
 
     KeyPresses=""
 
@@ -265,11 +262,15 @@ def drawStartScreen(Snek1,Snek2):
         Snek1.UpdateScreen()
         Snek2.UpdateScreen()
 
-        GameScreen.blit(titleWords,TitleRect)
-        GameScreen.blit(EnterMessage,EnterRect)
-        GameScreen.blit(QuitMessage,QuitRect)
-        StatButton = GameScreen.blit(StatsWords,StatsRect)
-        ControlsButton = GameScreen.blit(ControlsWords,ControlsRect)
+        titleWords.UpdateScreen()
+        EnterMessage.UpdateScreen()
+        QuitMessage.UpdateScreen()
+        StatsWords.Obj.bottomleft = (SettingsGap,StatsWords.Y)
+        ControlsWords.Obj.bottomleft = (SettingsGap, ControlsWords.Y)
+        #Credits.Obj.bottomleft = (SettingsGap,Credits.Y)
+        #Credits.UpdateScreen()
+        StatsWords.UpdateScreen()
+        ControlsWords.UpdateScreen()
         pygame.display.flip()
 
         for event in pygame.event.get():
@@ -298,66 +299,36 @@ def drawStartScreen(Snek1,Snek2):
                     terminate()
             elif event.type==MOUSEBUTTONUP and event.button==1:
                 MousePosition = pygame.mouse.get_pos()
-                if StatButton.collidepoint(MousePosition):
+                if StatsWords.Obj.collidepoint(MousePosition):
                     drawStatsScreen(Snek1,Snek2)
-                elif ControlsButton.collidepoint(MousePosition):
+                elif ControlsWords.Obj.collidepoint(MousePosition):
                     drawControlsScreen()
+                #elif Credits.Obj.collidepoint(MousePosition):
+                #    drawCredits(ScrollCounter=0,)
         
         if not GameMusicChannel.get_busy():
             GameMusicChannel.play(GameMusicSound)
 
 def drawStatsScreen(Snek1,Snek2):
-    MEGAFONT = pygame.font.Font("freesansbold.ttf",36)
+    MEGAFONT = 36
     Snek1HighScore,Snek2HighScore = GetGameFile()
 
-    HighScoreWords = MEGAFONT.render("HIGH SCORE", True, ORANGE)
-    HighScoreWordsRect = HighScoreWords.get_rect()
-    HighScoreWordsRect.center = ((int(GAMEWIDTH/2)),(GAMEHEIGHT - (GAMEHEIGHT/4)))
+    WindowOpener = Text("STATS WINDOW - ESC TO RETURN",RED,MEGAFONT,GAMEHEIGHT/12)
 
-    Snek1ScoreWords = BIGFONT.render(str("SNEK 1: " + Snek1HighScore),True,ORANGE)
-    Snek1ScoreRect = Snek1ScoreWords.get_rect()
-    Snek1ScoreRect.center = (int(GAMEWIDTH/2),(HighScoreWordsRect.bottom + Snek1ScoreRect.height))
-
-    Snek2ScoreWords = BIGFONT.render(str("SNEK 2: " + Snek2HighScore),True,ORANGE)
-    Snek2ScoreRect = Snek2ScoreWords.get_rect()
-    Snek2ScoreRect.center = (int(GAMEWIDTH/2),(Snek1ScoreRect.bottom + Snek2ScoreRect.height))
-
-    WindowOpener = BIGFONT.render("STATS WINDOW - ESC TO RETURN",True,RED)
-    WindowOpenerRect = WindowOpener.get_rect()
-    WindowOpenerRect.center = ((int(GAMEWIDTH/2)),30)
-
-    xSpacing = 10
-    ySpacing = 100
+    #Highscore stats:
+    HighScoreWords = Text("HIGH SCORE",ORANGE,MEGAFONT,WindowOpener.Obj.bottom + 3*WindowOpener.Obj.height)
+    Snek1ScoreWords = Text(str("SNEK 1: " + Snek1HighScore),ORANGE,BIGFONT,(HighScoreWords.Obj.bottom + HighScoreWords.Obj.height))
+    Snek2ScoreWords = Text(str("SNEK 2: " + Snek2HighScore),ORANGE,BIGFONT,Snek1ScoreWords.Obj.bottom + Snek1ScoreWords.Obj.height)
 
     #Stats for SNEK 1:
-    Snek1StatsWords = BIGFONT.render("STATS FOR SNEK 1:",True,Snek1.LableColor)
-    Snek1StatsRect = Snek1StatsWords.get_rect()
-    Snek1StatsRect.topleft = (xSpacing,ySpacing)
+    Snek1StatsWords = Text("SNEK 1 PREVIOUS GAME STATS:",Snek1.LableColor,BIGFONT,Snek2ScoreWords.Obj.bottom + 2*Snek2ScoreWords.Obj.height)
+    Snek1RedApplesWords = Text("RED Apples Eaten: " + str(Snek1.NormalApples),Snek1.LableColor,FONT,Snek1StatsWords.Obj.bottom + Snek1StatsWords.Obj.height)
+    Snek1YellowApplesWords = Text("YELLOW Apples Eaten: " + str(Snek1.SpeedApples),Snek1.LableColor,FONT,Snek1RedApplesWords.Obj.bottom+Snek1StatsWords.Obj.height)
 
-    ySpacing += 40
-    Snek1RedApplesWords = FONT.render("RED Apples Eaten: " + str(Snek1.NormalApples),True,Snek1.LableColor)
-    Snek1RedAppleRect = Snek1RedApplesWords.get_rect()
-    Snek1RedAppleRect.topleft = (xSpacing,ySpacing)
-    
-    ySpacing += 20
-    Snek1YellowApplesWords = FONT.render("YELLOW Apples Eaten: " + str(Snek1.SpeedApples),True,Snek1.LableColor)
-    Snek1YellowAppleRect = Snek1YellowApplesWords.get_rect()
-    Snek1YellowAppleRect.topleft = (xSpacing,ySpacing)
-
-    ySpacing += 60
-    Snek2StatsWords = BIGFONT.render("STATS FOR SNEK 2:",True,Snek2.LableColor)
-    Snek2StatsRect = Snek2StatsWords.get_rect()
-    Snek2StatsRect.topleft = (xSpacing,ySpacing)
-
-    ySpacing += 40
-    Snek2RedApplesWords = FONT.render("RED Apples Eaten: " + str(Snek2.NormalApples),True,Snek2.LableColor)
-    Snek2RedAppleRect = Snek1RedApplesWords.get_rect()
-    Snek2RedAppleRect.topleft = (xSpacing,ySpacing)
-
-    ySpacing += 20
-    Snek2YellowApplesWords = FONT.render("YELLOW Apples Eaten: " + str(Snek2.SpeedApples),True,Snek2.LableColor)
-    Snek2YellowAppleRect = Snek2YellowApplesWords.get_rect()
-    Snek2YellowAppleRect.topleft = (xSpacing,ySpacing)
+    #Stats for SNEK 2:
+    Snek2StatsWords = Text("SNEK 2 PREVIOUS GAME STATS:",Snek2.LableColor,BIGFONT,Snek1YellowApplesWords.Obj.bottom + 2*Snek1YellowApplesWords.Obj.height)
+    Snek2RedApplesWords = Text("RED Apples Eaten: " + str(Snek2.NormalApples),Snek2.LableColor,FONT,Snek2StatsWords.Obj.bottom + Snek2StatsWords.Obj.height)
+    Snek2YellowApplesWords = Text("YELLOW Apples Eaten: " + str(Snek2.SpeedApples),Snek2.LableColor,FONT,Snek2RedApplesWords.Obj.bottom+Snek2StatsWords.Obj.height)
 
     while True:
         for event in pygame.event.get(): 
@@ -370,59 +341,39 @@ def drawStatsScreen(Snek1,Snek2):
         drawGrid()
         Snek1.UpdateScreen()
         Snek2.UpdateScreen()
-        GameScreen.blit(WindowOpener,WindowOpenerRect)
-        GameScreen.blit(Snek1StatsWords,Snek1StatsRect)
-        GameScreen.blit(Snek2StatsWords,Snek2StatsRect)
-        GameScreen.blit(Snek1RedApplesWords,Snek1RedAppleRect)
-        GameScreen.blit(Snek1YellowApplesWords,Snek1YellowAppleRect)
-        GameScreen.blit(Snek2RedApplesWords,Snek2RedAppleRect)
-        GameScreen.blit(Snek2YellowApplesWords,Snek2YellowAppleRect)
-        GameScreen.blit(HighScoreWords,HighScoreWordsRect)
-        GameScreen.blit(Snek1ScoreWords,Snek1ScoreRect)
-        GameScreen.blit(Snek2ScoreWords,Snek2ScoreRect)
+        WindowOpener.UpdateScreen()
+        Snek1StatsWords.UpdateScreen()
+        Snek2StatsWords.UpdateScreen()
+        Snek1RedApplesWords.UpdateScreen()
+        Snek1YellowApplesWords.UpdateScreen()
+        Snek2RedApplesWords.UpdateScreen()
+        Snek2YellowApplesWords.UpdateScreen()
+        HighScoreWords.UpdateScreen()
+        Snek1ScoreWords.UpdateScreen()
+        Snek2ScoreWords.UpdateScreen()
 
         pygame.display.flip()
         if not GameMusicChannel.get_busy():
             GameMusicChannel.play(GameMusicSound)
 
 def drawControlsScreen():
-    ALMOSTMEDIUMFONT = pygame.font.Font("freesansbold.ttf",20)
-    MEDIUMFONT = pygame.font.Font("freesansbold.ttf",24)
-    WindowX = 10
-    WindowY =  100
-    WindowGap = 50
     #Messages to display:
-    WindowOpener = MEDIUMFONT.render("CONTROLS WINDOW - ESC TO RETURN",True,RED)
-    Snek1Keys = ALMOSTMEDIUMFONT.render("WASD To Control The BLUE Snek",True,CYAN) 
-    Snek2Keys = ALMOSTMEDIUMFONT.render("IJKL To Control The GREEN Snek",True,GREEN)
-    StartGame = ALMOSTMEDIUMFONT.render("ENTER OR SPACE To Start Game",True,ORANGE)
-    PauseGame = ALMOSTMEDIUMFONT.render("H To Pause/UnPause Game",True,ORANGE)
-        
-    WindowOpenerRect = WindowOpener.get_rect()
-    WindowOpenerRect.center = ((int(GAMEWIDTH/2)),30)
-    #Display Snek1 Message
-    Snek1KeysRect = Snek1Keys.get_rect()
-    Snek1KeysRect.topleft = (WindowX,WindowY)
-    #Display Snek2 Message
-    Snek2KeysRect = Snek2Keys.get_rect()
-    Snek2KeysRect.topleft = (WindowX,(WindowY + WindowGap))
-    #Display StartGame message
-    StartGameRect = StartGame.get_rect()
-    StartGameRect.topleft = (WindowX,(WindowY + (2 * WindowGap)))
-    #Display PauseGame message
-    PauseGameRect = PauseGame.get_rect()
-    PauseGameRect.topleft = (WindowX,(WindowY + (3 * WindowGap)))
+    WindowOpener = Text("CONTROLS - ESC TO RETURN",RED,36,GAMEHEIGHT/10,int(GAMEWIDTH/2))
+    Snek1Keys = Text("WASD To Control The BLUE Snek",CYAN,BIGFONT,2*WindowOpener.Obj.bottom + WindowOpener.Obj.height) 
+    Snek2Keys = Text("IJKL To Control The GREEN Snek",GREEN,BIGFONT,Snek1Keys.Obj.bottom + 2*Snek1Keys.Obj.height)
+    StartGame = Text("ENTER OR SPACE To Start Game",ORANGE,BIGFONT,Snek2Keys.Obj.bottom + 2*Snek2Keys.Obj.height)
+    PauseGame = Text("H To Pause/UnPause Game",ORANGE,BIGFONT,StartGame.Obj.bottom + 2*StartGame.Obj.height)
 
     while True:
         GameScreen.fill(BLACK)
         drawGrid()
         Snek1.UpdateScreen()
         Snek2.UpdateScreen()
-        GameScreen.blit(WindowOpener,WindowOpenerRect)
-        GameScreen.blit(Snek1Keys,Snek1KeysRect)
-        GameScreen.blit(Snek2Keys,Snek2KeysRect)
-        GameScreen.blit(StartGame,StartGameRect)
-        GameScreen.blit(PauseGame,PauseGameRect)
+        WindowOpener.UpdateScreen()
+        Snek1Keys.UpdateScreen()
+        Snek2Keys.UpdateScreen()
+        StartGame.UpdateScreen()
+        PauseGame.UpdateScreen()
         pygame.display.flip()
 
         for event in pygame.event.get(): 
@@ -436,7 +387,7 @@ def drawControlsScreen():
             GameMusicChannel.play(GameMusicSound)
 
 def drawCredits(ScrollCounter):
-    ScrollSpeed = 2 * ScrollCounter
+    ScrollSpeed = 5*ScrollCounter
 
     #If there is an easier way of doing this, it will need to be redone
     #Bigninning of the pain:
@@ -448,42 +399,56 @@ def drawCredits(ScrollCounter):
     NameColor = PURPLE
 
     CreditsList = []
-    CreditsList.append(Text("CREDITS",HeaderColor,58,GAMEHEIGHT))
+    CreditsList.append(Text("CREDITS",CYAN,58,GAMEHEIGHT - ScrollSpeed))
     HeaderSpace = HeaderGap + CreditsList[-1].Obj.height + CreditsList[-1].Y
+
+    CreditsList.append(Text("Dual Sneks - BrokenKeyboard Studios",HeaderColor,HeaderSize,HeaderSpace))
+    HeaderSpace = NameGap + CreditsList[-1].Y
+
     CreditsList.append(Text("ENGINEERS",HeaderColor,HeaderSize,HeaderSpace))
     NameSpace = NameGap + CreditsList[-1].Y
+
     CreditsList.append(Text("Woldorf",NameColor,NameSize,NameSpace))
-    #CreditsList.append(Text("ARTISTS",ORANGE,HeaderSize,HeaderSpace))
-    #CreditsList.append(Text("Woldorf",,NameSize,NameSpace))
     HeaderSpace = HeaderGap + CreditsList[-1].Y
+
     CreditsList.append(Text("DEVELOPERS",HeaderColor,HeaderSize,HeaderSpace))
     NameSpace = NameGap + CreditsList[-1].Y
+
     CreditsList.append(Text("Woldorf",NameColor,NameSize,NameSpace))
     HeaderSpace = HeaderGap + CreditsList[-1].Y
+
     CreditsList.append(Text("ORIGINAL CONCEPT",HeaderColor,HeaderSize,HeaderSpace))
     NameSpace = NameGap + CreditsList[-1].Y
+
     CreditsList.append(Text("Woldorf",NameColor,NameSize,NameSpace))
     NameSpace = NameGap + CreditsList[-1].Y
+
     CreditsList.append(Text("01c",NameColor,NameSize,NameSpace))
     HeaderSpace = HeaderGap + CreditsList[-1].Y
+
     CreditsList.append(Text("SPECIAL THANKS TO",HeaderColor,HeaderSize,HeaderSpace))
     NameSpace = NameGap + CreditsList[-1].Y
+
     CreditsList.append(Text("Brambrew",NameColor,NameSize,NameSpace))                 
     NameSpace = NameGap + CreditsList[-1].Y
-    CreditsList.append(Text("KelserVin",NameColor,NameSize,NameSpace))                #WIll need double checked
+
+    CreditsList.append(Text("KelserVin",NameColor,NameSize,NameSpace))               
     NameSpace = NameGap + CreditsList[-1].Y
-    CreditsList.append(Text("Froststormer",NameColor,NameSize,NameSpace))             #Will need double checked
-    HeaderSpace = HeaderGap + CreditsList[-1].Y
-    CreditsList.append(Text("ESC TO EXIT CREDITS",RED,HeaderSize,HeaderSpace))
+
+    CreditsList.append(Text("Froststormer",NameColor,NameSize,NameSpace))             
+    HeaderSpace = 2 * HeaderGap + CreditsList[-1].Y
+
+    CreditsList.append(Text("ESC TO RETURN",RED,HeaderSize,HeaderSpace))
     #CreditsList.append(Text("",ORANGE,HeaderSize,HeaderSpace))
     #CreditsList.append(Text("",BLUE,NameSize,NameSpace))
     #NameSpace = NameGap + CreditsList[-1].Y
     #HeaderSpace = HeaderGap + CreditsList[-1].Y
 
     for Name in CreditsList:
-        Name.UpdateScreen(ScrollSpeed)
-
-    return CreditsList
+        if Name == CreditsList[-1] and Name.Y <= GAMEHEIGHT/2:
+            Name.Y = GAMEHEIGHT/2
+        Name.Obj.center = (Name.X,Name.Y)
+        Name.UpdateScreen()
 
 def terminate():
     pygame.quit()
@@ -493,13 +458,13 @@ def makeFruits():
     x=random.randint(0, int(GAMEWIDTH / CELLSIZE)-1)
     y=random.randint(0, int(GAMEHEIGHT / CELLSIZE)-1)
 
-    if random.randint(0,1000) == 1:
+    if random.randint(0,250) == 1:
         Type = "Master"
     else:
         TypeList=["Normal","Speed","Normal"]
         Type = random.choice(TypeList)
 
-    return {'x':x,'y':y,'Type':"Master"}
+    return {'x':x,'y':y,'Type':"Type"}
 
 def KonamiChecker(KeyPresses,TicksPerSec):
     #Konami Code: wUaLsDdR
